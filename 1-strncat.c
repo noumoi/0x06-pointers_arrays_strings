cf8d5c4a034bf1b448a398bfbf3c24507cb3261f
noumoi <holawaleh.software@gmail.com>
2023-09-22 12:19:07 -0700

    1-strncat.c

---
+++ 1-strncat.c
@@ -0,0 +1,25 @@
+#!/bin/bash
+#include "main.h"
+/**
+ *_strcat - function that concatenates two strings.
+ *@dest: pointer to destination string.
+ *@src: pointer to source strings
+ *Return: pointer to destination string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int midas, j;
+
+	midas = 0;
+	while (dest[midas] != '\0')
+	{
+		midas++;
+	}
+	for (j = 0; src[j] != '\0'; j++, midas++)
+	{
+		dest[midas] = src[j];
+	}
+	dest[midas] = '\0';
+	return (dest);
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+pointers 6
